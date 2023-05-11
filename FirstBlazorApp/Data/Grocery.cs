f429eaffe8a8285ebcdbd5ae25bebdb79ecaa248
Dan <dantdamgaard@hotmail.com>
2023-05-11 13:23:43 +0200

    first push

---
+++ FirstBlazorApp/Data/Grocery.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace FirstBlazorApp.Data
+{
+    public class Grocery
+    {
+        public Grocery(string name, float price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        [Required]
+        [StringLength(15, ErrorMessage ="Name should be less then 15 charactors.")]
+        public string Name { get; set; }
+        [Required]
+        [Range(0, 10000, ErrorMessage ="valid price range is (1-00000).")]
+        public float Price { get; set; }
+    }
+}
